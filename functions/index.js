5ed56d1638fd6e01088a50cb3beb82ad11a5eaad
Kevin <kevin_jpm@hotmail.com>
2022-06-25 19:57:22 -0500

    API Rest v1.1.0

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const functions = require("firebase-functions");
-const express = require('express')
-const admin = require('firebase-admin')
+const express = require('express');
+const cors = require('cors');
+const admin = require('firebase-admin');
 
 const app= express();
 
@@ -9,6 +10,7 @@ admin.initializeApp({
     databaseURL: "https://hf-trazabilidad-89c0e-default-rtdb.firebaseio.com" 
 })
 
+app.use(cors());
 
 app.use(require('./routes/cosecha.routes'))
 
